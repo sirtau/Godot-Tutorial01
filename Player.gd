07b62035de38a03849705da0bc48136c92797a6f
sirtau <contact@hamidmousa.com>
2021-08-22 08:48:08 +1000

    Coin Score connected, now increases on coin pickup
    Added UI script to set the score based on the paramater passed in set_score_text(score).

--- Player.gd
+++ Player.gd
@@ -8,7 +8,7 @@ var gravity : int = 800
 
 var vel : Vector2 = Vector2()
 
-
+onready var ui : Node = get_node("/root/MainScene/CanvasLayer/UI")
 onready var sprite : Sprite = get_node("Sprite")
 
 func _physics_process(delta):
@@ -36,3 +36,4 @@ func die ():
 	
 func collect_coin (value):
 	score += value
+	ui.set_score_text(score)
